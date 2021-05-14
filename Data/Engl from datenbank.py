d5a3e3377e515cbc8a41c9cd4a7269647fec9954
Chris Simeon <christian-herberholz@hotmail.de>
2021-05-14 16:19:57 +0200

    Datenverzeichnis Variabel

--- Data/Engl from datenbank.py
+++
@@ -1,6 +0,0 @@
-import LarousseEngl as LE
-import sqlite3
-
-Datenbankpfad = "/Users/chris/Documents/OneDrive/Christian/Sprachen/ProgrammVokabeln/Datenbank/Vokabldatebank.sqlite3"
-conn = sqlite3.connect(Datenbankpfad)
-c = conn.cursor()
--- Data/Import_from_Kindle.py
+++
@@ -1,9 +0,0 @@
-import DB_Functions as DB
-import Kindle_Functions
-
-buff = DB.get_Vokabeln()
-
-if buff ==0:
-    Kindle_Functions.Kindle_leeren()
-else:
-    print("Fehler, Kindle nicht geleert")
